4a2b85b49118ec28f31b34b70aedcaa1fa151ff9
zephrous <danielpoh3131@gmail.com>
2023-12-08 22:06:55 +0800

    update new

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "@/components/HeroSection";
+import Project from "@/components/Project";
 
 export default function Home() {
   return(
     <div className="flex min-h-screen flex-col">
       <HeroSection />
+      <Project/>
     </div>
   );
 }
