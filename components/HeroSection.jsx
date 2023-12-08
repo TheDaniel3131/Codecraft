b94d96dcf50624f4337778934b0e404dc90ce2a2
zephrous <danielpoh3131@gmail.com>
2023-12-08 19:54:21 +0800

    test

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import TypeWritter from "typewriter-effect";
 
 const HeroSection = () => {
-    return(
-        <main className="py-16 mt-16 md:flex md: shirink-0"></main>
+    return (
+        <main className="py-16 mt-16 md:flex md:shirink-0">
             <div className="grid grid-cols-1 md:grid-cols-12">
                 <div className="col-span1 md:col-span-8 place-self-center justify-self-start">
                     <h1 className="fold-extrabold">
                         <span className="text-white text-8xl">
-
+                            {/* Add your content here */}
                         </span>
                     </h1>
                 </div>
             </div>
+        </main>
     );
 };
 
