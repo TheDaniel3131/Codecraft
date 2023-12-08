82c7e58cf5b7d755f2b32bd501852c8052adc38c
zephrous <danielpoh3131@gmail.com>
2023-12-08 19:50:08 +0800

    new

---
+++ components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TypeWritter from "typewriter-effect";
+
+const HeroSection = () => {
+    return(
+        <main className="py-16 mt-16 md:flex md: shirink-0"></main>
+            <div className="grid grid-cols-1 md:grid-cols-12">
+                <div className="col-span1 md:col-span-8 place-self-center justify-self-start">
+                    <h1 className="fold-extrabold">
+                        <span className="text-white text-8xl">
+
+                        </span>
+                    </h1>
+                </div>
+            </div>
+    );
+};
+
+export default HeroSection;
