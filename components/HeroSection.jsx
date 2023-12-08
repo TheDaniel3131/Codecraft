76d77fc21ac31706fd3ffe1a9069cf31bf735372
zephrous <danielpoh3131@gmail.com>
2023-12-08 21:11:08 +0800

    update hero!

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TypeWritter from "typewriter-effect";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
     return (
@@ -14,12 +15,12 @@ const HeroSection = () => {
                 >
                     <h1 className="font-extrabold">
                         <span className="text-white text-8xl">
-                            Hi, I&apos;m{" "}
+                            Hello, I&apos;m{" "}
                             <span className="text-transparent bg-clip-text sm:text-8xl md:text-9xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
                                 Zephrous
                             </span>
                         </span>
-                        <span className="text-6xl">
+                        <span className="text-5xl">
                             <TypeWritter
                                 options={{
                                     autoStart: true,
@@ -27,7 +28,7 @@ const HeroSection = () => {
                                 }}
                                 onInit={(typewriter) => {
                                     typewriter
-                                    .typeString('<span style="color: #FF77FF">Web Developer</span>')
+                                    .typeString('<span style="color: #4F46E5">Web Developer</span>')
                                     .pauseFor(1500)
                                     .deleteAll()
                                     .typeString('<span style="color: #9E7BFF">Software Engineer</span>')
@@ -37,6 +38,13 @@ const HeroSection = () => {
                                 }}
                             ></TypeWritter>
                         </span>
+                        <p className="text-sky-100 mt-4 text-base sm:text-lg md:text-xl mb-6">Slide down to know more about me!</p>
+                        <Link 
+                            href="https://drive.google.com/file/d/1KLTQOu91ZDNuBv3he3hEzrNRc2L96Ro3/view?usp=sharing" target="_blank" 
+                            rel="noopener noreferrer"
+                            className="px-10 inline-block py-4 rounded-full border-4 border-pink-500 hover:bg-pink-500">
+                                View Resume
+                        </Link>
                     </h1>
                 </motion.div>
             </div>
