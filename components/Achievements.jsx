ff4b07f38f8399b4f20566f2dd1c7658c1e72738
zephrous <danielpoh3131@gmail.com>
2023-12-10 01:27:14 +0800

    last push

---
+++ components/Achievements.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const achievementsList = [
+    {
+        metric: "Projects",
+        value: "10+",
+    },
+    {
+        metric: "Years of Experience",
+        value: "4",
+    },
+    {
+        metric: "Certifications",
+        value: "5",
+    },
+];
+
+const Achievements = () => {
+    return (
+        <div className="xl:gap-4 sm:py-4 xl:px-16 mb-20" id="achievements">
+            <h1 className="text-center text-5xl font-bold mb-16" style={{ letterSpacing: "0.05em" }}>
+                Achievements
+            </h1>
+            <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between text-center" style={{ width: "80%", margin: "0 auto", textAlign: "center" }}>
+                {achievementsList.map((achievement, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="flex flex-col items-center justify-center mx-6 text-center items-center justify-between" // Add text-center class here
+                        >
+                            <h2 className="text-white text-4xl font-bold" style={{ letterSpacing: "0.1em" }}>
+                                {achievement.value}
+                            </h2>
+                            <p className="text-[#ADB7BE] text-base" style={{ letterSpacing: "0.1em" }}>
+                                {achievement.metric}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+export default Achievements;
--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,16 +3,33 @@ import TypeWritter from "typewriter-effect";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
+import dynamic from "next/dynamic";
 
 const HeroSection = () => {
     return (
-        <main className="py-16 mt-16 md:flex md:shrink-0">
+        <main className="py-16 mt-16 md:flex md:shrink-0" id="main"> 
+            <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="absolute inset-0 w-full h-full object-cover z-0 max-w-[1920px] transform scale-150"
+                style={{
+                    filter: "brightness(10%)",
+                    backgroundSize: "cover",
+                    backgroundAttachment: "center",
+                    position: "absolute"
+                }}
+            >
+                <source src="/Hero-BG.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className="grid grid-cols-1 md:grid-cols-12">
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
+                    animate={{ opacity: 1, scale: 0.9 }}
                     transition={{ duration: 0.5 }}
-                    className="col-span-1 md:col-span-8 place-self-center justify-self-start">
+                    className="col-span-12 md:col-span-8 place-self-center">
                     <h1 className="font-extrabold mb-8">
                         <span className="text-white text-8xl">
                             Hello, I&apos;m{" "}
@@ -20,7 +37,7 @@ const HeroSection = () => {
                                 Zephrous
                             </span>
                         </span>
-                        <span className="text-5xl mt-4 block">
+                        <span className="text-5xl mt-6 block">I&apos;m a
                             <TypeWritter
                                 options={{
                                     autoStart: true,
@@ -34,32 +51,50 @@ const HeroSection = () => {
                                         .typeString('<span style="color: #9E7BFF">Software Engineer</span>')
                                         .pauseFor(1500)
                                         .deleteAll()
+                                        .typeString('<span style="color: #FFB800">Graphic Designer</span>')
+                                        .pauseFor(1500)
+                                        .deleteAll()
+                                        .typeString('<span style="color: #00C853">Chess Player</span>')
+                                        .pauseFor(1500)
+                                        .deleteAll()
+                                        .typeString('<span style="color: #FF4081">CTF Player</span>')
+                                        .pauseFor(1500)
+                                        .deleteAll()
+                                        .typeString('<span style="color: #243c5a">Student</span>')
+                                        .pauseFor(1500)
+                                        .deleteAll()
+                                        .typeString('<span style="color: #00BFA5">AI Enthusiast</span>')
+                                        .pauseFor(1500)
+                                        .deleteAll()
                                         .start();
                                 }}
                             ></TypeWritter>
                         </span>
-                        <p className="text-sky-100 mt-8 text-base sm:text-lg md:text-xl mb-6">
-                            Slide down to know more about me!
+                        <p className="text-sky-100 mt-10 text-base sm:text-lg md:text-xl mb-2 font-semibold">
+                            Also known as &quot;Zeph&quot;, you can call me Daniel.
+                        </p>
+                        <p className="text-sky-100 text-base sm:text-lg md:text-xl mb-6 font-semibold">
+                            Scroll down below to find out more about me.
                         </p>
                         <Link
                             href="https://drive.google.com/file/d/1KLTQOu91ZDNuBv3he3hEzrNRc2L96Ro3/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="px-10 inline-block py-4 rounded-full border-4 border-pink-500 hover:bg-pink-500">
+                            className="mt-4 px-10 inline-block py-4 rounded-full border-4 border-blue-500 hover:bg-blue-500 text-lg font-bold text-2xl" // Updated font size to text-xl
+                        >
                             View Resume
                         </Link>
                     </h1>
                 </motion.div>
-                <div className="col-span-4 md:col-span-4 place-self-center mt-12 lg:mt-0 pt-0 lg:pt-24">
-                    <div className="w-[400px] h-[400px] relative z-0">
+                <div className="col-span-12 md:col-span-4 place-self-center mt-12 lg:mt-0 pt-0 lg:pt-24">
+                    <div className="w-[600px] h-[600px] relative z-0">
                         <Image
                             src="/Hero-Section.png"
-                            alt="Hero Section"
-                            width={400}
-                            height={400} 
-                            className="transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                        >
-                        </Image>
+                            alt="hero section"
+                            className=" transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={450}
+                            height={450}
+                        />
                     </div>
                 </div>
             </div>
@@ -67,4 +102,5 @@ const HeroSection = () => {
     );
 };
 
-export default HeroSection;
+export default dynamic(() => Promise.resolve(HeroSection), {ssr: false});
+
