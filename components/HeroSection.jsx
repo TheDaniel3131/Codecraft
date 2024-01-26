4c91f1201ca0b2b9c9252e097764a3777c33e94f
zephrous <danielpoh3131@gmail.com>
2024-01-26 15:39:10 +0800

    updated linear gradient to text

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -16,7 +16,6 @@ const HeroSection = () => {
             className="col-span-1 md:col-span-8 place-self-center justify-self-start"
           >
             <h1 className="font-extrabold">
-              {/* span text separator -- for design*/}
               <span className="text-white text-8xl ">
                 Hello, I&apos;m{" "} {/* Escape the single quote */}
                 <span className="text-transparent bg-clip-text sm:text-8xl md:text-9xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
